f723b10e7bcc24d7830b572584750472501e0172
galihbaskara25GB <galihbaskara25.gb@gmail.com>
2020-11-23 16:00:19 +0700

    Revert "Add Factory for Blogs and Campaign"
    This reverts commit 7a372a3287c7b6ba7538f67cd9a1090db0864c39.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
@@ -13,6 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\User::class, 2)->create();
+        
     }
 }
